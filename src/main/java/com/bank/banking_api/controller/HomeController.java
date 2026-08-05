8d370b5c4e69b2c20f34877ac887ec1c396412f3
ahmedali0822 <shaikmohammadfardhinkhan@gmail.com>
2026-08-05 17:01:12 +0530

    Add root health check controller

---
+++ src/main/java/com/bank/banking_api/controller/HomeController.java
@@ -0,0 +1,13 @@
+package com.bank.banking_api.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    @GetMapping("/")
+    public String home() {
+        return "Banking API is live and operational!";
+    }
+}
