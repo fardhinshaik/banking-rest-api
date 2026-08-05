11db9d635e50daa9289063798a5997fc969ef8aa
ahmedali0822 <shaikmohammadfardhinkhan@gmail.com>
2026-08-05 21:36:05 +0530

    Redirect home endpoint to Swagger UI

--- src/main/java/com/bank/banking_api/controller/HomeController.java
+++ src/main/java/com/bank/banking_api/controller/HomeController.java
@@ -1,13 +1,13 @@
 package com.bank.banking_api.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class HomeController {
 
     @GetMapping("/")
     public String home() {
-        return "Banking API is live and operational!";
+        return "redirect:/swagger-ui/index.html";
     }
 }
